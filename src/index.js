ad51e40b1543b8471bea593577e7fd7dd906ce99
alinaYamkova <alyamkova@yahoo.com>
2020-12-09 22:35:19 +0200

    фыксить хомпейдж

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import menuBar from './templates/menu-theme.hbs'
 const ulList = document.querySelector('.js-menu');
 const newLiItem = menuBar(menuData);
 const checkBox = document.querySelector('#theme-switch-toggle');
-const body = document.body;
 ulList.insertAdjacentHTML('afterbegin', newLiItem);
 
 const Theme = {
@@ -13,25 +12,43 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+const replaceTheme = (oldTheme, newTheme) => {
+  document.body.classList.remove();
+  document.body.classList.add();
+  return;  
+}
+
+const body = document.body;
+
 checkBox.addEventListener('change', onThemeChange);
+
 function onThemeChange(evt) {
   evt.preventDefault();
 
+  // if (event.target.checked) {
+  //   replaceTheme(Theme.LIGHT, Theme.DARK);
+  //   localStorage.setItem('theme', Theme.DARK);
+  // } else {
+  //   replaceTheme(Theme.DARK, Theme.LIGHT);
+  //   localStorage.setItem('theme', Theme.LIGHT);
+  // }
+
   if (event.target.checked) {
-    body.classList.toggle(Theme.DARK); 
+    body.classList.add(Theme.DARK); 
     body.classList.remove(Theme.LIGHT);
-    localStorage.setItem('theme', 'dark-theme');
+    localStorage.setItem('theme', Theme.DARK);
   } else {
-    body.classList.toggle('light-theme');
+    body.classList.add(Theme.LIGHT);
     body.classList.remove(Theme.DARK);
-    localStorage.setItem('theme', 'light-theme');
+    localStorage.setItem('theme', Theme.LIGHT);
   }
 };
 
 function savedThemeColor() {
   const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark-theme') {
-    body.classList.add(Theme.DARK);
+  if (savedTheme === Theme.DARK) {
+    body.classList.add(Theme.DARK); 
+    // replaceTheme(Theme.LIGHT, Theme.DARK);
     checkBox.checked = true;
   }
 };
